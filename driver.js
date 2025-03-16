09b3abbc794fca01f95c74ddec4eeb42abb0c1b6
220100413 <ma552156@gmail.com>
2025-03-17 01:53:09 +0200

    Update driver.js

--- driver.js
+++ driver.js
@@ -28,32 +28,33 @@ function unblind(blindingFactor, sig, n) {
 
 let agency = new SpyAgency();
 
-//
-// ***YOUR CODE HERE***
-//
-// Prepare 10 documents with 10 different cover identities (using the makeDocument function).
-// Blind each of the 10 documents, and remember to store their blinding factors.
-//
+let coverNames = ["Agent X", "Agent Y", "Agent Z", "Agent A", "Agent B", "Agent C", "Agent D", "Agent E", "Agent F", "Agent G"];
+let documents = coverNames.map(makeDocument);
+let blindDocs = [];
+let blindingFactors = [];
 
+documents.forEach((doc, index) => {
+  let { blinded, r } = blind(doc, agency.n, agency.e);
+  blindDocs.push(blinded);
+  blindingFactors.push(r);
 
 agency.signDocument(blindDocs, (selected, verifyAndSign) => {
 
-  //
-  // ***YOUR CODE HERE***
-  //
-  // The 'signDocument' function takes a callback function, which
-  // specifies which of the 10 documents the spy agency will sign.
-  //
-  // You must call the 'verifyAndSign' function, specifying arrays with:
-  // 1) the blinding factors
-  // 2) the original documents
-  //
-  // Note that you should specify this information for all documents
-  // EXCEPT the specified document.  (In the selected position, set
-  // these positions to 'undefined'.)
-  //
-  // The verifyAndSign function will return the blinded signature.
-  // Unblind it, and make sure that the signature is valid for
-  // the selected document.
+let blindedFactorsForVerification = blindingFactors.map((bf, i) => (i === selected ? undefined : bf));
+  
+  let originalDocsForVerification = documents.map((doc, i) => (i === selected ? undefined : doc));
+  
+  let blindedSignature = verifyAndSign(blindedFactorsForVerification, originalDocsForVerification);
+  
+  let signature = unblind(blindingFactors[selected], blindedSignature, agency.n);\
+  
+  let isValid = blindSignatures.verify({
+    
+    unblinded: signature,
+    N: agency.n,
+    E: agency.e,
+    message: documents[selected],
+  });
 
+  console.log(`Signature valid for selected document: ${isValid}`);
 });
